ce13e0ea47293c74113f1d268e45ecb5acc3af3e
JohnnyC365 <jcherubini365@gmail.com>
2025-01-24 10:35:19 -0500

    Update lab2c.py

---
+++ lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Assign command-line arguments to variables
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+# Print the output with the exact required formatting
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
