d33f2550b671bcd9106447f75d515720e34968aa
JohnnyC365 <jcherubini365@gmail.com>
2025-01-24 10:35:54 -0500

    Update lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+!/usr/bin/env python3
+
+# Author: John Cherubini
+# Author ID: jacherubini
+# Date Created: 2025/01/24
+
+# Initialize the timer variable
+timer = 10
+
+# While loop to count down from 10 to 1
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+# Print blast off message
+print("blast off!")
